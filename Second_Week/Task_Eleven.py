fed1dde464cb488785c02877e5e1635a7132e43d
Cronix Raimer <ayubovbakhtiyor@gmail.com>
2023-06-22 16:31:51 +0400

    comments have been added on Task Eleven as explanation

--- Second_Week/Task_Eleven.py
+++ Second_Week/Task_Eleven.py
@@ -1,10 +1,13 @@
 def supperDigit(n, k):
     #...
 
-    if len(str(n)) == 1:
+    if len(str(n)) == 1: #check is length of my string one digit if yes return supperDigit
         return n
     else:
         digit_sum = str(sum([int(i) for i in n])) * k
+        #str (sum(int(converting my string into integer then telling give me the sum of my string in length )))
+        #then multipling it and getting all loop getting back same subsequence which is sum so until will not reach on digit
+        
         return supperDigit(digit_sum, 1)
 
     #Iteration over each digit in the integer
