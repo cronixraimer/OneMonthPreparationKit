3a8c2cf3014cfd1442e8497f23903c1a12dad0ed
Cronix Raimer <ayubovbakhtiyor@gmail.com>
2023-06-29 00:15:23 +0400

    Test Room

--- First_Week/Task_Three.py
+++ First_Week/Task_Three.py
@@ -4,17 +4,19 @@ import random
 import re
 import sys
 
-s = input()
+s = input() #input from user
 
 
 
-hours, minutes, seconds = map(int, s[:-2].split(':'))
+hours, minutes, seconds = map(int, s[:-2].split(':')) # spliting the user input by : into
+#hours, minutes and seconds converting it map and where also spliting by -2
 
-if s[-2:] == "AM" and hours == 12:
-    hours -= 12
+if s[-2:] == "AM" and hours == 12: #if check is our last 2 input AM  and hour equal to 12
+    hours -= 12 # dicreasing it by 12 where our time from midnight till noon
 
-if s[-2:] == "PM" and hours != 12:
-    hours += 12
+if s[-2:] == "PM" and hours != 12: #if check is our last input 12 PM and is not 12
+    hours += 12 #increasing our time input by 12 where will be converted to more then 12PM
+    #like 13, 14 and so on
 formatted_time = "{:02d}:{:02d}:{:02d}".format(hours, minutes, seconds)
 
 print(formatted_time)
--- Third_Week/Test_Room.py
+++ Third_Week/Test_Room.py
@@ -1,3 +1,15 @@
-weight = input('Enter your weight: ')
-sum = int(weight) * 0.453592
-print(sum)
+#weight = input('Enter your weight: ')
+#sum = int(weight) * 0.453592
+#print(sum)
+
+#for i in range(3):
+#    for j in range(2):
+#        print(f'({i}, {j})')
+
+number = [5, 2, 5, 2, 2]
+
+for i in number:
+    output = ''
+    for j in range(i):
+        output += 'X'
+    print(output)
