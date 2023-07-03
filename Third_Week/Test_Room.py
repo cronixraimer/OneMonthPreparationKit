013bbcfd2c052d1c9821b7ed93db57cbc2c49712
Cronix Raimer <ayubovbakhtiyor@gmail.com>
2023-07-03 13:19:09 +0400

    Modiefied on Test_Room learning on practice

--- Third_Week/Test_Room.py
+++ Third_Week/Test_Room.py
@@ -6,10 +6,12 @@
 #    for j in range(2):
 #        print(f'({i}, {j})')
 
-number = [5, 2, 5, 2, 2]
+#number = [5, 2, 5, 2, 2]
 
-for i in number:
-    output = ''
-    for j in range(i):
-        output += 'X'
-    print(output)
+#for i in number:
+#    output = ''
+#    for j in range(i):
+#        output += 'X'
+#    print(output)
+list_numbers = [5, 2, 4, 22, 44, 55, 22]
+print(max(list_numbers))
